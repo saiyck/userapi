90608a808f66ddb0b6b57015d92a16d39fb44992
“saiyck” <asanjay723@gmail.com>
2023-03-17 11:58:14 -0700

    course issue fixes

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,15 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
+    app.enableCors({
+        origin: [
+            'http://localhost:3000',
+            'https://mongodbweb.onrender.com',
+            'https://mongodbweb-app.vercel.app'
+        ],
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+        credentials: true,
+    });
     await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin : [
+      'http://localhost:3000',
+      'https://mongodbweb.onrender.com',
+      'https://mongodbweb-app.vercel.app'
+    ],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  });
   await app.listen( process.env.PORT || 3001);
 }
 bootstrap();
